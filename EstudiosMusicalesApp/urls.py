9077d55034e0fe7f97a5ed7d1d7dca8afc33807c
coco-fernandez <elo.arg.88@gmail.com>
2022-07-07 00:44:40 -0300

    con Template index funcionando

---
+++ EstudiosMusicalesApp/urls.py
@@ -0,0 +1,10 @@
+from operator import index
+from django.contrib import admin
+from django.urls import path,include
+from .views import *
+
+
+urlpatterns = [
+    path('',index,name="index"),    
+    
+]
--- EstudiosMusicalesApp/views.py
+++ EstudiosMusicalesApp/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def inicio(request):
-    return HttpResponse("Hola")
+def index(request):
+    return render(request,"EstudiosMusicalesApp/index.html",)
