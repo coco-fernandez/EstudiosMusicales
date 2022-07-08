123e9f39af7fb9ef103d79851dcfb4dd64315855
coco-fernandez <elo.arg.88@gmail.com>
2022-07-07 22:16:17 -0300

    clases creadas en models

--- EstudiosMusicalesApp/models.py
+++ EstudiosMusicalesApp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Estudio(models.Model):
+    nombre=models.CharField(max_length=30)
+    ubicacion=models.CharField(max_length=30)
+    cantidad_salas=models.IntegerField()
+    
+
+class Banda(models.Model):
+    nombre=models.CharField(max_length=30)
+    genero=models.CharField(max_length=30)
+    cantidad_integrantes=models.IntegerField()
+    
+
+class Productor(models.Model):
+    nombre=models.CharField(max_length=30)
+    apellido=models.CharField(max_length=30)
+    email=models.EmailField()
+    
+
+
