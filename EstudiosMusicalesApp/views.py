0a7e110b915932d01550641336e65697773d4946
coco-fernandez <elo.arg.88@gmail.com>
2022-07-07 22:23:10 -0300

    vistas creadas en views

--- EstudiosMusicalesApp/views.py
+++ EstudiosMusicalesApp/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from EstudiosMusicalesApp.models import Banda, Estudio, Productor
+
 
 def index(request):
     return render(request,"EstudiosMusicalesApp/index.html",)
+
+def estudios(request):
+    estudios=Estudio.objects.all()
+    ctx={"estudios":estudios}
+    return render(request,'ProyectoCoderApp/estudios.html',ctx)
+    pass
+
+def productores(request):
+    productores=Productor.objects.all()
+    ctx={"productores":productores}
+    return render(request,'ProyectoCoderApp/productores.html',ctx)
+    pass
+
+def bandas(request):
+    bandas=Banda.objects.all()
+    ctx={"bandas":bandas}
+    return render(request,'ProyectoCoderApp/bandas.html',ctx)
+    pass
+
